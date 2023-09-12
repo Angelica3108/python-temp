982b02f6333942672a1ace0bbc8cabc9a3486946
Angelica3108 <angelica.neves@geb.inatel.br>
2023-09-12 11:27:29 +0200

    Python moule for Raspyberry

--- temp.py
+++ temp.py
@@ -3,8 +3,21 @@
 
 TEMP_PATH="/sys/class/thermal/thermal_zone0/"
 
-fo = open(TEMP_PATH + "temp", "r")
-temp = fo.read()
+# fo = open(TEMP_PATH + "temp", "r")
+# temp = fo.read()
+# print(temp)
 
-print(temp)
+def readTemp():
+    fo = open(TEMP_PATH + "temp", "r")
+    temp = fo.read()
+    return int(temp)
 
+def averageTemp(n):
+    sum=0
+    for i in range(n):
+        sum=sum+readTemp()
+        average = sum/n
+    return average
+
+newTemp = averageTemp(4)
+print(newTemp)
